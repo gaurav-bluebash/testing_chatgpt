44e44316a2a96185efc213ecb7a3378ee062baa7
harsh <harsh.kumar@bluebash.co>
2023-03-20 15:35:55 +0530

    Send response to front-end

--- app/controllers/chatgpt_controller.rb
+++ app/controllers/chatgpt_controller.rb
@@ -1,4 +1,5 @@
 class ChatgptController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
 	def generate
     client = OpenAI::Client.new(access_token: "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")
@@ -7,10 +8,13 @@ class ChatgptController < ApplicationController
         model: "gpt-3.5-turbo",
         messages: [{ role: "user", content: "#{params["search"]} + only html code using inline css"}],
     })
+    response = response["choices"][0]["message"]["content"]
     file = Tempfile.new([[*'a'..'z',*'A'..'Z'].shuffle[0..6].join,'.html'])
-    file.write(response["choices"][0]["message"]["content"])
+    file.write(response)
     file.rewind
-    system("google-chrome #{file.path}")
+    # system("google-chrome #{file.path}")
+    debugger
+    render json: {content: response, file: file.path}
 	end
 
   def suggest
